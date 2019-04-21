687cfbc2234cbee5b2abc53f644c3c29fdfb1bfc
Munsy <tmunson@wsu.edu>
2019-04-20 20:00:51 -0700

    progress bar

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 
-const cure = require('./cure')
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -59,3 +57,4 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+const cure = require('./cure')
