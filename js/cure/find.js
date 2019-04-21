0d2d5845e456462620da42f2335170f9b60ef3bd
Munsy <tmunson@wsu.edu>
2019-04-21 14:08:46 -0700

    update cure.js

--- js/cure/find.js
+++ js/cure/find.js
@@ -12,16 +12,32 @@ angular.module('cure').controller("findController", ['$scope', '$http', '$cookie
 		if($location.path() != "/find/act") {
 			return;
 		}
+
 		$scope.Find.Act = true;
 		$scope.Find.Addon = false;
+
+		$http.get("http://localhost:8080/find/act")
+		.then(function(response) {
+			$scope.Find.Data = response.data;
+		}, function(response) {
+			$scope.Find.Data = response.data;
+		});
 	};
 
 	const findAddon = function() {
 		if($location.path() != "/find/addon") {
 			return;
 		}
+		
 		$scope.Find.Act = true;
 		$scope.Find.Addon = false;
+
+		$http.get("http://localhost:8080/find/addon")
+		.then(function(response) {
+			$scope.Find.Data = response.data;
+		}, function(response) {
+			$scope.Find.Data = response.data;
+		});
 	};
 
 	$scope.ChangeView = function(view) {
--- js/cure/install.js
+++ js/cure/install.js
@@ -1,36 +1,66 @@
 angular.module('cure').controller("installController", ['$scope', '$http', '$cookies', '$location', function($scope, $http, $cookies, $location) {
 	$scope.Install = {};
+	$scope.Install.Error = false;
 	$scope.Install.Act = false;
 	$scope.Install.Addon = false;
 	$scope.Install.Installing = false;
+	$scope.Install.ChooseLocation = false;
+
 	$scope.Install.Progress = {};
+	$scope.Install.Progress.CurrentFile = "";
 	$scope.Install.Progress.Current = 0;
 	$scope.Install.Progress.Total = 0;
 
 	const clear = function() {
+		$scope.Install.Progress.CurrentFile = "";
 		$scope.Install.Progress.Current = 0;
 		$scope.Install.Progress.Total = 0;
 		$scope.Install.Act = false;
 		$scope.Install.Addon = false;
 		$scope.Install.Installing = false;
+		$scope.Install.ChooseLocation = false;
 	}
 
 	const installAct = function() {
 		if($location.path() != "/install/act") {
 			return;
 		}
-		$scope.Install.Act = true;
-		$scope.Install.Addon = false;
-		$scope.Install.Installing = false;
-	};
+					
+		$scope.Install.Installing = true;
 
-	const installAddon = function() {
-		if($location.path() != "/install/addon") {
+		while($scope.Install.Installing) {		
+			$http.get("http://localhost:8080/install/act/progress")
+			.then(function(response) {
+				$scope.Install.Progress.Current = response.data.Current;
+				$scope.Install.Progress.Total = response.data.Total;
+				if($scope.Install.Progress.Total == 100) {
+					$scope.Install.Installing = false;
+				}
+			}, function(response) {
+				$scope.Install.Data = response.data;
+				$scope.Install.Installing = false;
+			});
+		}
+	}
+
+	const installActView = function() {
+		if($location.path() != "/install/act") {
 			return;
 		}
 		$scope.Install.Act = true;
 		$scope.Install.Addon = false;
 		$scope.Install.Installing = false;
+		$scope.Install.ChooseLocation = true;
+	};
+
+	const installAddonView = function() {
+		if($location.path() != "/install/addon") {
+			return;
+		}
+		$scope.Install.Act = false;
+		$scope.Install.Addon = true;
+		$scope.Install.Installing = false;
+		$scope.Install.ChooseLocation = true;
 	};
 
 	$scope.ChangeView = function(view) {
@@ -40,11 +70,19 @@ angular.module('cure').controller("installController", ['$scope', '$http', '$coo
 		installAddon();
 	};
 
-	$scope.InstallAct = function() {
+	$scope.Retry = function(view) {
+		$scope.Install.Error = false;
+		$location.path(view)
+		clear();
+		installAct();
+		installAddon();
+	}
 
+	$scope.InstallAct = function() {
+		getProgress();
 	}
 
 	clear();
-	installAct();
-	installAddon();
+	installActView();
+	installAddonView();
 }]);
