1ffec77cbf3a2c619c7c04612a4a2f374c386a08
Munsy <tmunson@wsu.edu>
2019-04-21 11:45:19 -0700

    update cure.js, main.js

--- cure.js
+++ cure.js
@@ -8,54 +8,76 @@ const bp = require('body-parser');
 const express = require('express');
 const web = express();
 
-let actPathDefault = "C:\\Program Files (x86)\\Advanced Combat Tracker"
+class Cure {
+  constructor(httpPort) {
+    this.Engine = {};
+    this.Engine.Http = {};
+    this.Engine.Http.server = {};
+    this.Engine.Http.port = 0;
+    this.Engine.Http.port = httpPort;
 
-web.use(bp.json());
-web.use(bp.urlencoded({ extended: false }));
-web.use(cp());
-web.use(cors());  
+    this.Data = {};
+    this.Data.curePathDefault = "C:\\Program Files (x86)\\Cure";
+    this.Data.cureTemp = this.Data.curePathDefault + "\\.temp";
+    this.Data.actPathDefault = "C:\\Program Files (x86)\\Advanced Combat Tracker";
+    this.Data.actDownloadURL = "https://advancedcombattracker.com/includes/page-download.php?id=57"
 
-web.get('/search', function(request, response) {
-  let filesArr = {};
-
-  find.file(/\.html$/, actPathDefault, function(files) {
-    filesArr = files;
-    response.send(filesArr);
-    return;
-  })
-  .error(function(err) {
-    let e = new Error('Not Found');
-    e.status = 404;
-    response.statusCode = 404;
-    response.end();
-  });
-});
-
-web.get('/install', function(request, response) {
-  const file = fs.createWriteStream("act.zip");
-  const request = http.get("https://advancedcombattracker.com/includes/page-download.php?id=57", function(response) {
-    response.pipe(file);
-  });
-
-  ncp(source, destination, function (err) {
-    if (err) {
-      let e = new Error('Not Found');
-      e.status = 404;
-      response.statusCode = 404;
-      response.end();
-    }
-    console.log('done!');
-  });
+    web.use(bp.json());
+    web.use(bp.urlencoded({ extended: false }));
+    web.use(cp());
+    web.use(cors());
+    
+    web.get('/search', function(request, response) {
+      let filesArr = {};
+      find.file(/\.html$/, this.Data.actPathDefault, function(files) {
+        filesArr = files;
+        response.send(filesArr);
+        return;
+      })
+      .error(function(err) {
+        let e = new Error('Not Found');
+        e.status = 404;
+        response.statusCode = 404;
+        response.end();
+      });
+    });
+    
+    web.get('/install/act', function(request, response) {
+      if (!fs.existsSync(this.Data.cureTemp)) {
+        fs.mkdirSync(this.Data.cureTemp);
+      }
   
-});
+      if (!fs.existsSync(this.Data.actPathDefault)) {
+        fs.mkdirSync(this.Data.actPathDefault);
+      }
+  
+      const file = fs.createWriteStream(this.Data.cureTemp + "\\act.zip");
+      const request = http.get(this.Data.actDownloadURL, function(response) {
+        response.pipe(file);
+      });
+      ncp(source, destination, function (err) {
+        if (err) {
+          let e = new Error('Not Found');
+          e.status = 404;
+          response.statusCode = 404;
+          response.end();
+        }
+      });
+    });
+    
+    web.use(function(request, response, next) {
+      let err = new Error('Not Found');
+      err.status = 404;
+    });
+    
+    web.set('port', this.Engine.Http.port);
+    
+    this.Engine.Http.server = http.createServer(web);
+  }
 
-web.use(function(request, response, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-});
+  Listen() {
+    this.Engine.Http.server.listen(this.Engine.Http.port);
+  }
+}
 
-web.set('port', 8080);
-
-const server = http.createServer(web);
-
-server.listen(8080);
+module.exports = Cure;
--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function createWindow () {
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Turn off the menu.
   mainWindow.setMenu(null)
@@ -57,4 +57,8 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-const cure = require('./cure')
+const Cure = require('./cure')
+
+const cure = new Cure(8080);
+
+cure.Listen();
